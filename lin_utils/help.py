8e770e71e76a893dbdbdc2d5f30ef1998990171f
balsam <miscaccs@protonmail.com>
2020-10-26 04:08:25 -0700

    Fix typo

--- lin_utils/help.py
+++ lin_utils/help.py
@@ -2,7 +2,7 @@ from discord import Embed
 from lin_utils.clientwide import prefixes
 
 help_embed = Embed(color=0x77dd77, title=':beginner: linbot help',
-                   descrption='This is the testing version of linbot.')
+                   description='This is the testing version of linbot.')
 
 help_embed.add_field(name='Prefix', value=f'The current prefix is set to `{prefixes[0]}`.',
                      inline=False)
